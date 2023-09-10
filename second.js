103deb0a3f0ed03eec65cf77559ff960792a9d80
anilghale1 <ghalepasang5@gmail.com>
2023-09-10 11:55:15 +0545

    learned padding margin concept in css

---
+++ second.js
@@ -0,0 +1,25 @@
+
+// let person = {
+    
+// };
+
+// person.naam = 'anil '; 
+
+// person.umer = 22;
+
+// document.write(person.naam);
+// document.write(person.umer);
+ 
+
+//  function display()
+//  {
+
+//  var a = document.getElementById('box').value;
+//  var b = document.getElementById('box').value;
+//   document.write(a+b);
+// }
+
+const headin = document.querySelector('.heading');
+headin.innerHTML = 'Web dev is superb';
+headin.style.color = 'blue';
+headin.style.fontSize = '100px';
