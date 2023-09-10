574078bbf0145a647d4f4920daff7a9146130eed
anilghale1 <ghalepasang5@gmail.com>
2023-09-10 11:58:32 +0545

    learned DOM in js

---
+++ JS_Practise/largeNumber.js
@@ -0,0 +1,37 @@
+
+let a = document.getElementById('numOne');
+let b = document.getElementById('numTwo');
+let c = document.getElementById('numThree');
+
+
+let result = document.getElementById("result");
+ 
+ function input()
+ {
+    let x = parseInt(a.value);
+    let y = parseInt(b.value);
+    let z = parseInt(c.value);
+
+      compare(x,y,z);
+
+ }
+
+ function compare(g,h,i)
+ {
+    if(g>h && g>i)
+    {
+        result.innerHTML = "Greatest number : " +g;
+    }  
+    
+    else if(h>i)
+    {
+        result.innerHTML = "Greatest number : " +h;
+    }
+
+    else{
+        result.innerHTML = "Greatest number : " +i;
+    }
+
+
+ }
+
