b8343d5126753a727755ab2e271abf6aecf78b54
anilghale1 <ghalepasang5@gmail.com>
2023-09-10 11:53:08 +0545

    first class on js

---
+++ First.js
@@ -0,0 +1,19 @@
+document.write("Hello Duniya  ");
+console.log("Mero duniya beglaixa");
+
+let a = 5;
+var b = 10;
+
+ document.write(a+b);
+
+   function changeText() {
+      document.getElementById("demo").textContent = "First JavaScript";
+   }
+
+   document.getElementById("demo").document.write("First JavaScript");
+  
+
+// function displayMessage() {
+//     document.write("First JavaScript");
+//   }
+  
