f00687091d6e127694bec232a7f25de487981b58
anilghale123 <ghalepasang5@gmail.com>
2024-01-11 23:10:52 +0545

    updated arraySum

--- JS_Practise/arraySum.js
+++ JS_Practise/arraySum.js
@@ -1,26 +1,54 @@
-let arr = [1, 1, 0, 0, 1];
+let arr = [2, 1, 0, 3, 0];
 let arr2 = [];
 let arr3 = [];
 let arr4 = [];
 let sum = 0;
 let sum2 = 0;
 let sum3 = 0;
+var arrayOfDigits;
+var arrayOfDigits2;
+var arrayOfDigits3;
 
-for (let i = 0; i < arr.length -1; i++) {
-   sum = arr[i] + arr[i + 1]; 
-   arr2.push(sum);
-}
-   for (let i = 0; i < arr2.length -1; i++) {
-   sum2 = arr2[i] + arr2[i + 1];
-   arr3.push(sum2);
+// First loop
+for (let i = 0; i < arr.length - 1; i++) {
+   sum = arr[i] + arr[i + 1];
+   
+   
+   if (sum >= 10) {
+    arrayOfDigits = Array.from(String(sum), Number);
+      arr2.push(arrayOfDigits);
+   } else {
+      arr2.push(sum);
    }
+}
 
-   for (let i = 0; i < arr3.length -1; i++) {
-    sum3 = arr3[i] + arr3[i + 1];
-    arr4.push(sum3);
-    }
+// Second loop
+for (let i = 0; i < arr2.length - 1; i++) {
+  
+      sum2 = arr2[i] + arr2[i+1];
+      if (sum2 >= 10) {
+        arrayOfDigits2 = Array.from(String(sum2), Number);
 
-console.log(arr2); 
+         arr3.push(arrayOfDigits2);
+      } else {
+         arr3.push(sum2);
+      }
+   } 
+
+
+// Third loop
+for (let i = 0; i < arr3.length - 1; i++) {
+ 
+      sum3 = arr3[i] + arr3[i+1];
+      if (sum3 >= 10) {
+        arrayOfDigits3 = Array.from(String(sum3), Number);
+
+         arr4.push(arrayOfDigits3);
+      } else {
+         arr4.push(sum3);
+      }
+   } 
+
+console.log(arr2);
 console.log(arr3);
 console.log(arr4);
-
